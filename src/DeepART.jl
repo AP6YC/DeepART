b4c3ff8762b73150cce02fe512afe389df849077
Sasha Petrenko <sap625@umsystem.edu>
2024-02-08 14:36:23 -0600

    Init Julia project for packaging driver src

---
+++ src/DeepART.jl
@@ -0,0 +1,36 @@
+"""
+    DeepART.jl
+
+# Description
+Definition of the `DeepART` module, which encapsulates experiment driver code.
+
+# Authors
+- Sasha Petrenko <petrenkos@mst.edu> @AP6YC
+"""
+
+"""
+A module encapsulating the experimental driver code for the DeepART project.
+
+# Imports
+
+The following names are imported by the package as dependencies:
+$(IMPORTS)
+
+# Exports
+
+The following names are exported and available when `using` the package:
+$(EXPORTS)
+"""
+module DeepART
+
+# -----------------------------------------------------------------------------
+# DEPENDENCIES
+# -----------------------------------------------------------------------------
+
+# Full usings (which supports comma-separated import notation)
+using
+    AdaptiveResonance,
+    DocStringExtensions,
+    DrWatson
+
+end
