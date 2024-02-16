7695bafc05aa40a0f5d14502ed8fb2f909735d66
Sasha Petrenko <sap625@umsystem.edu>
2024-02-16 10:54:47 -0600

    Docs make and serve

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,85 @@
-using Documenter
+"""
+    make.jl
+
+# Description
+This file builds the documentation for the `CFAR` project using Documenter.jl and other tools.
+
+# Authors
+- Sasha Petrenko <petrenkos@mst.edu>
+"""
+
+# -----------------------------------------------------------------------------
+# DEPENDENCIES
+# -----------------------------------------------------------------------------
+
+using
+    Documenter,
+    # DemoCards,
+    Logging,
+    Pkg
+
+# -----------------------------------------------------------------------------
+# SETUP
+# -----------------------------------------------------------------------------
+
+# Common variables of the script
+PROJECT_NAME = "DeepART"
+DOCS_NAME = "docs"
+
+# Fix GR headless errors
+ENV["GKSwstype"] = "100"
+
+# Get the current workind directory's base name
+current_dir = basename(pwd())
+@info "Current directory is $(current_dir)"
+
+# If using the CI method `julia --project=docs/ docs/make.jl`
+#   or `julia --startup-file=no --project=docs/ docs/make.jl`
+if occursin(PROJECT_NAME, current_dir)
+    push!(LOAD_PATH, "../src/")
+# Otherwise, we are already in the docs project and need to dev the above package
+elseif occursin(DOCS_NAME, current_dir)
+    Pkg.develop(path="..")
+# Otherwise, building docs from the wrong path
+else
+    error("Unrecognized docs setup path")
+end
+
+# Inlude the local package
 using DeepART
+
+# using JSON
+if haskey(ENV, "DOCSARGS")
+    for arg in split(ENV["DOCSARGS"])
+        (arg in ARGS) || push!(ARGS, arg)
+    end
+end
+
+# -----------------------------------------------------------------------------
+# GENERATE
+# -----------------------------------------------------------------------------
+
+# using Documenter
+# using DeepART
+
+assets = [
+    joinpath("assets", "favicon.ico"),
+]
 
 makedocs(
     sitename = "DeepART",
-    format = Documenter.HTML(),
-    modules = [DeepART]
+    authors="Sasha Petrenko",
+    # format = Documenter.HTML(),
+    format=Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true",
+        assets = assets,
+        size_threshold = Int(1e6),
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+    modules = [DeepART],
+    repo = "https://github.com/AP6YC/DeepART/blob/{commit}{path}#L{line}",
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
@@ -13,3 +88,14 @@ makedocs(
 #=deploydocs(
     repo = "<repository url>"
 )=#
+
+# -----------------------------------------------------------------------------
+# DEPLOY
+# -----------------------------------------------------------------------------
+
+deploydocs(
+    repo="github.com/AP6YC/DeepART.git",
+    # devbranch="develop",
+    devbranch="main",
+    # push_preview = should_push_preview(),
+)
