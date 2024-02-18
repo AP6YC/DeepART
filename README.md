1c48674e2ea792bfb2a7fbd0f6f804c6782cd516
Sasha Petrenko <sap625@umsystem.edu>
2024-02-17 21:59:19 -0600

    EOB commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # DeepART
+
+TODO:
+
+1. Simple deep art
+   1. SGD
+   2. SGD + EWC
+   3. Hebbian
+   4. Instar
+2. Deep features and deep weights
+3. L2metrics subpackage?
+4. Create package logo
--- src/lib/models/3_deep.jl
+++ src/lib/models/3_deep.jl
@@ -6,3 +6,115 @@
 # Description
 TODO
 """
+
+"""
+"""
+const DEFAULT_N_SHARED = Vector{Int}([10, 20])
+
+"""
+"""
+const DEFAULT_N_HEADS = Vector{Int}([20. 10])
+
+"""
+"""
+@with_kw struct opts_MultiHeadField
+    """
+    """
+    n_shared::Vector{Int} = DEFAULT_N_SHARED
+
+    """
+    """
+    n_headS::Vector{Int} = DEFAULT_N_HEADS
+end
+
+"""
+Container for a multihead [`DeeperART`](@ref) neural network field.
+"""
+struct MultiHeadField{T <: Flux.Chain}
+    """
+    The shared layers.
+    """
+    shared::T
+
+    """
+    The heads of the network.
+    """
+    heads::Vector{T}
+end
+
+"""
+"""
+function add_head!(model::MultiHeadField)
+    local_head = Chain(
+		Dense(size_tuple[1]=>120, sigmoid),
+		Dense(84=>10, sigmoid),
+		# softmax
+	)
+    push!(model.heads, local_head)
+
+    # Explicitly empty return
+    return
+end
+
+function MultiHeadField()
+    shared = Chain(
+		Dense(size_tuple[1]=>120, sigmoid),
+		Dense(120=>84, sigmoid),
+		Dense(84=>10, sigmoid),
+		# softmax
+	)
+
+    heads = []
+    for ix = 1:5
+
+    end
+
+    return MultiHeadField(
+        shared,
+        heads,
+    )
+end
+
+"""
+Options container for a [`DeeperART`](@ref) module.
+"""
+@with_kw struct opts_DeeperART
+    rho::Float = 0.6
+end
+
+"""
+Stateful information of a DeeperART module.
+"""
+struct DeeperART{T <: Flux.Chain}
+    """
+    Feature presentation layer.
+    """
+    F1::T
+
+    """
+    Feedback expectancy layer
+    """
+    F2::T
+
+    """
+    Options container
+    """
+    opts::opts_DeeperART
+end
+
+"""
+Keyword argument constructor for a [`DeeperART`](@ref) module passing the keyword arguments to the [`opts_DeeperART`](@ref) for the module.
+
+# Arguments
+- `kwargs...`: the options keyword arguments.
+"""
+function DeeperART(;kwargs...)
+    # Create the options from the keyword arguments
+    opts = opts_DeeperART(;kwargs...)
+
+    # Instantiate and return a constructed module
+    return DeeperART(
+        opts,
+    )
+end
+
