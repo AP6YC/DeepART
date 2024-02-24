a83db01ae09fda0d82730450212a9bf9294d4f80
Sasha Petrenko <sap625@umsystem.edu>
2024-02-24 17:55:12 -0600

    Fix grads EWC usage

--- scripts/0_init/ewc_dev.jl
+++ scripts/0_init/ewc_dev.jl
@@ -9,6 +9,7 @@ using Revise
 using Flux
 using DeepART
 using Parameters
+using ProgressMeter
 
 # Make a simple multilayer perceptron
 model = Chain(
@@ -24,42 +25,61 @@ data = all_data["moon"]
 mnist = DeepART.get_mnist()
 # function train(model, data, params)
 #     opt = Descent(params.learning_rate)
-
 #     for (x, y) in data
 #         gradient = Flux.gradient(() -> loss(x, y), Flux.params(model))
 #         update!(opt, Flux.params(model), gradient)
 #     end
-
 #     return model
 # end
 
 """
 Function to update Fisher Information Matrix based on a Flux model's gradients.
 """
-function update_fim!(FIM, gradients)
+function update_fim(FIM, gradients)
     alpha = 0.1
     if isnothing(FIM)
-        FIM = Dict(key => (g .* g) for (key, g) in gradients)
+        # FIM = Dict(key => (g .* g) for (key, g) in gradients)
+        # FIM = []
+        # for layer in gradients
+        #     push!(FIM, layer .* layer)
+        # end
+        new_FIM = [layer .* layer for layer in gradients]
+        # new_FIM = [layer .* layer for layer in gradients]
     else
-        for (key, g) in gradients
-            FIM[key] = (1 - alpha) * FIM[key] + alpha * (g .* g)
-        end
+        # for (key, g) in gradients
+        #     FIM[key] = (1 - alpha) * FIM[key] + alpha * (g .* g)
+        # end
+        # for ix = 1:length(gradients)
+        # for ix in eachindex(gradients)
+        #     new_FIM[ix] = (1 - alpha) * FIM[ix] + alpha * (gradients[ix] .* gradients[ix])
+        # end
+        new_FIM = [(1 - alpha) * FIM[ix] + alpha * (gradients[ix] .* gradients[ix]) for ix in eachindex(gradients)]
     end
+    return new_FIM
 end
 
 """
 Function to update model parameters with respect to the old Fisher Information Matrix.
 """
 function update_ewc!(model, FIM, lambda, mu)
-    for (key, param) in Flux.params(model)
-        param .-= (lambda / 2) * (param .- mu[key]) .* FIM[key]
+    # for (key, param) in Flux.params(model)
+    #     param .-= (lambda / 2) * (param .- mu[key]) .* FIM[key]
+    # end
+    n_layers = length(Flux.params(model))
+    # for ix in eachindex(Flux.params(model))
+    for ix = 1:n_layers
+        local_FIM = FIM[ix]
+        # Flux.params(model)[ix] .-= (lambda / 2) * (Flux.params(model)[ix] .- mu[ix]) .* FIM[ix]
+        Flux.params(model)[ix] .-= (lambda / 2) * (Flux.params(model)[ix] .- mu[ix]) .* local_FIM
     end
 end
 
-"""
-Simple crossentropy loss function.
-"""
-loss(x, y) = Flux.crossentropy(model(x), y)
+# """
+# Simple crossentropy loss function.
+# """
+# loss(x, y) = Flux.crossentropy(model(x), y)
+
+loss(x, y) = Flux.crossentropy(x, y)
 
 """
 EWC training routine.
@@ -70,14 +90,26 @@ function train(
     params,
 )
     # Define an empty Dict for storing mu values
-    mu_dict = Dict()
+    # mu_dict = Dict()
     opt = Descent(params.learning_rate)
+    s = Flux.setup(opt, model);
 
     # Fisher information matrix approximation diagonal
     FIM = nothing
+    # mu = nothing
+    mu = copy(Flux.params(model))
 
-    for (x, y) in data
-        gradient = Flux.gradient(() -> loss(x, y), Flux.params(model))
+    # for (x, y) in data
+    n_data = length(data.y)
+    @showprogress for ix  = 1:n_data
+        x = reshape(data.x[:, :, ix], 784)
+        @info size(x)
+        y = data.y[ix]
+        # gradient = Flux.gradient(() -> loss(x, y), Flux.params(model))
+        grads = Flux.gradient(model) do m
+            result = m(x)
+            loss(result, y)
+        end
 
         # # Get the previous model parameters if available
         # if haskey(mu_dict, :previous_model_params)
@@ -87,27 +119,38 @@ function train(
         #     end
         # end
 
-        update_fim!(FIM, gradient)
-        update_ewc!(model, FIM, params.lambda, mu_dict)
+        FIM = update_fim(FIM, grads[1])
+        # @info FIM
+        update_ewc!(model, FIM, params.lambda, mu)
 
+        mu = copy(Flux.params(model))
         # mu_dict[:previous_model_params] = [copy(param) for param in Flux.params(model)]
         # mu_dict = Dict(key => )
 
-        update!(opt, Flux.params(model), gradient)
+        Flux.update!(s, model, grads[1])
     end
 
     return model
 end
 
 """
-Parameters struct
+EWC parameters struct.
 """
-@with_kw struct Parameters
-    learning_rate::Float64
-    lambda::Float64
+@with_kw struct EWCParameters
+    learning_rate::Float64 = 0.01
+    lambda::Float64 = 0.1
 end
 
-params = Parameters(0.01, 0.1)
+params = EWCParameters()
+
+ix = 1
+x = reshape(mnist.train.x[:, :, ix], 784)
+y = mnist.train.y[ix]
+# gradient = Flux.gradient(() -> loss(x, y), Flux.params(model))
+grads = Flux.gradient(model) do m
+    result = m(x)
+    loss(result, y)
+end
 
 # Train the model
-trained_model = train(model, data, params)
+trained_model = train(model, mnist.train, params)
