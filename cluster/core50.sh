f5e4992195b28371916667a0dbb31b03fb2dd020
Sasha Petrenko <sap625@umsystem.edu>
2024-03-25 19:01:11 -0500

    Fix core50 script

--- cluster/core50.sh
+++ cluster/core50.sh
@@ -1,1 +1,4 @@
-wget --directory-prefix=$DIR'/../data/work/core50' http://bias.csr.unibo.it/maltoni/download/core50/core50_128x128.zip
+# Setup
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+mkdir $DIR/../work/data/core50
+wget --directory-prefix=$DIR'/../work/data/core50' http://bias.csr.unibo.it/maltoni/download/core50/core50_128x128.zip
