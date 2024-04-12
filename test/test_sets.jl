4cfb638bb98978968a7878d40f0df543a49ba012
Sasha Petrenko <sap625@umsystem.edu>
2024-04-12 12:15:30 -0500

    More test dev

--- test/test_sets.jl
+++ test/test_sets.jl
@@ -11,3 +11,22 @@ using Logging
 
 # Set the log level
 LogLevel(Logging.Info)
+
+@testset "Data" begin
+    @info "----------------- DATA -----------------"
+    # Accept data downloads
+    ENV["DATADEPS_ALWAYS_ACCEPT"] = true
+    # Fix plotting on headless
+    ENV["GKSwstype"] = "100"
+    # Dataset selection
+    DATASET = "mnist"
+    N_TRAIN = 100
+    N_TEST = 100
+
+    data = DeepART.load_one_dataset(
+        DATASET,
+        n_train=N_TRAIN,
+        n_test=N_TEST,
+    )
+    fdata = DeepART.flatty(data)
+end
